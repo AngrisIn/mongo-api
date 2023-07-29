8d842ef551ac0e8f7dd73a3c3e076e67479da2ab
tharunoptimus <tharunoptimus@outlook.com>
2023-07-30 04:26:28 +0530

    Added duration field to video schema with default as 1.5

--- schemas/VideoSchema.js
+++ schemas/VideoSchema.js
@@ -5,7 +5,8 @@ const VideoSchema = new Schema({
     uuid: { type: String, required: true, unique: true },
     episodeSynopsis: { type: String, required: true },
     episodeType: { type: String, required: true },
-    episodeTitle: { type: String, required: true }
+    episodeTitle: { type: String, required: true },
+    duration: { type: String, required: true, default: '1.5' },
 
 }, { timestamps: true });
 
